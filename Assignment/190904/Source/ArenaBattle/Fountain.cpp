bf60888e3c88d1763d7eb0682995125507892004
Hyeonjung Kim <40892893+ECLvisltion@users.noreply.github.com>
2019-09-04 15:30:17 +0900

    Add files via upload

---
+++ Assignment/190904/Source/ArenaBattle/Fountain.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Fountain.h"
+
+
+// Sets default values
+AFountain::AFountain()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Water"));
+
+	RootComponent = Body; // 루트(근본) 설정
+	Water->SetupAttachment(Body); // 부모관계 (Body - Water)
+
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 140.0f));
+}
+
+// Called when the game starts or when spawned
+void AFountain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFountain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
