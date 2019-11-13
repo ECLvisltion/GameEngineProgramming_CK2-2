202b7a09fb4e871215337b143d62dc4879846d87
Hyeonjung Kim <eclvisltion@gmail.com>
2019-11-13 15:54:38 +0900

    191113

---
+++ Class/191113/ArenaBattle/Source/ArenaBattle/BTService_Detect.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTService_Detect.h"
+
+UBTService_Detect::UBTService_Detect()
+{
+}
+
+/*void UBTService_Detect::TickNode(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	//APawn * ControllingPawn = OwnerComp.GetAIOwner
+	return;
+}*/
